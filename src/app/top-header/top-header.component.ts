a4926fcd1941c497a32cfe9cd4de0c81368bc7a0
kunjc2lbiz <kunj.shah@c2lbiz.com>
2024-10-11 23:24:27 +0530

    understand how data binding is done using html tags, html attribyte, css style/id

--- src/app/top-header/top-header.component.ts
+++ src/app/top-header/top-header.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'top-header',
+  // selector: '[top-header]', // using it as html attribute 
+  // selector: '.top-header', // using it as css class/id. use . or #
   templateUrl: './top-header.component.html',
   styleUrl: './top-header.component.css'
 })
