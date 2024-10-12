25d8bc961d224cc805e4ebea0f7c06a80cd98dac
kunjc2lbiz <kunj.shah@c2lbiz.com>
2024-10-12 18:50:33 +0530

    understand event binding more precisely using add cart increment or decrement buttons

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -3,25 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'product-list',
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
-  name : string ="Kunj";
-  //  addToCart:number = 0;
+  // name : string ="Kunj";
+  addToCart: number = 0;
   product = {
-     name: 'iPhone X',
-     price: 789,
-     color: 'Black',
-     discount: 8.5,
-     inStock: 10,
-     pImage: '/assets/images/iphone.png'
-  }
+    name: 'iPhone X',
+    price: 789,
+    color: 'Black',
+    discount: 8.5,
+    inStock: 10,
+    pImage: '/assets/images/iphone.png',
+  };
 
   getDiscountedPrice() {
-    return this.product.price - (this.product.price * this.product.discount / 100)
+    return (
+      this.product.price - (this.product.price * this.product.discount) / 100
+    );
   }
 
-  onNameChange(e : any){
-    this.name = e.target.value;
+  onNameChange(event: any) {
+    // this.name = e.target.value;
+    // console.log(this.name);
+  }
+
+  decrementCartValue() {
+    if (this.addToCart > 0) {
+      this.addToCart--;
+    }
+  }
+
+  incrementCartValue() {
+    if (this.addToCart < this.product.inStock) {
+      this.addToCart++;
+    }
   }
 }
